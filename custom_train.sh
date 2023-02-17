cb6f38122d34d2b27edec2776e44b535180289fb
aimspot <82668230+aimspot@users.noreply.github.com>
2023-02-17 15:28:33 +0300

    up

---
+++ custom_train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#example: bash -l custom_train.sh yolov8 True
+
+if [ "$1" = "yolov5" ] || [ "$1" = "yolov7" ] || [ "$1" = "yolov8" ]; then
+    source /home/farm/anaconda3/bin/activate $1
+    if [ "$2" ]; then
+        python custom_train_all.py --arch $1 --split True
+    else
+        python custom_train_all.py --arch $1
+    fi
+else
+    echo "Enter one of possible options {yolov5/yolov7/yolov8}"
+fi
