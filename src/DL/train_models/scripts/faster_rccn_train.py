84c11c935b2bcde814b7c96ffa6d521db9f31612
aimspot <som_nncs@list.ru>
2024-06-25 14:11:22 +0300

    update rcnn and ssd

--- src/DL/train_models/scripts/faster_rccn_train.py
+++ src/DL/train_models/scripts/faster_rccn_train.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import subprocess
 
 
 def train_frcnn(CONFIG_PATH, EPOCHS, BATCH_SIZE, GPU_COUNT, IMG_SIZE):
@@ -7,8 +8,10 @@ def train_frcnn(CONFIG_PATH, EPOCHS, BATCH_SIZE, GPU_COUNT, IMG_SIZE):
     Runs faster-rccn training using the parameters specified in the config.
     """
     file = Path(__file__).resolve()
+    os.environ['WANDB_MODE'] = 'disabled'
+
     command = "python3" if GPU_COUNT == 0 else f"OMP_NUM_THREADS=1 python3 -m torch.distributed.run --nproc_per_node {GPU_COUNT}"
-    os.system(
+    full_command = (
         command +
         f' {file.parents[1]}/models/fastercnn-pytorch-training-pipeline/train.py'
         f" --data {CONFIG_PATH}"
@@ -16,3 +19,14 @@ def train_frcnn(CONFIG_PATH, EPOCHS, BATCH_SIZE, GPU_COUNT, IMG_SIZE):
         f" --batch {BATCH_SIZE}"
         f" --model fasterrcnn_resnet50_fpn" +
         f" --name {os.path.dirname(CONFIG_PATH)}")
+    process = subprocess.Popen(full_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        output = process.stdout.readline()
+        if output == b'' and process.poll() is not None:
+            break
+        if output:
+            print(output.decode().strip())
+
+    rc = process.poll()
+    return rc
