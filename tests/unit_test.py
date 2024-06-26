8bbc8233dd80d462d420223bdba77262cc55cedf
aimspot <som_nncs@list.ru>
2024-06-26 17:27:06 +0300

    fix unit-test

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -4,21 +4,27 @@ import sys
 project_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(os.path.dirname(project_dir)))
 from ODRS.src.data_processing.ml_processing.recommendation_module import predict_models
-
+from ODRS.src.data_processing.data_utils.utils import load_class_names, get_models, get_data_path
 
 class TestDatasetInfo(unittest.TestCase):
-    def test_dataset_info(self):
-        dataset_path = "/home/runner/work/ODRS/src/user_datasets/WaRP/Warp-D"
+
+    def test_load_classes(self):
         classes_path = "/home/runner/work/ODRS/src/classes.txt"
-        run_path = '/home/runner/work/ODRS/src/'
-
-        result = predict_models(dataset_path, classes_path, run_path)
-
+        result = load_class_names(classes_path)
         self.assertIsInstance(result, list)
-        self.assertEqual(len(result), 3)
-        self.assertIsInstance(result[0], int)
-        self.assertIsInstance(result[1], int)
-        self.assertIsInstance(result[2], int)
+        self.assertEqual(len(result), 28)
+
+    
+    def test_models(self):
+        result = get_models()
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 15)
+
+    def test_data_path(self):
+        default = '/home/runner/work/ODRS/ODRS/user_datasets/WaRP/Warp-D'
+        result = get_data_path(default)
+        self.assertEqual(result, default)
+
 
 
 if __name__ == "__main__":
